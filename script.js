9fb8966415682ef6cebb86213edc1a14990cd0a8
Siddharth Gade <siddharthrgade21@gmail.com>
2026-03-28 08:19:03 +0530

    How Fast Do You Think?
    This project is a fast-thinking reaction game where users must respond to random instructions as quickly as possible. It measures reaction speed, shows the final thinking time, and gives a result based on performance. A creative JavaScript project focused on speed and accuracy. 💻⚡

---
+++ script.js
@@ -0,0 +1,19 @@
+body {
+    background-color: black;
+    color: white;
+    text-align: center;
+    font-family: Arial;
+}
+
+button {
+    padding: 15px 30px;
+    font-size: 20px;
+    background-color: red;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+h1 {
+    margin-top: 100px;
+}
